c154078a4e84f44f950e3d3300b61226dee8be91
dubeyAditya <sinister.dubey@gmail.com>
2023-07-11 02:42:24 +0530

    Add Auth Module

--- libs/shared/src/auth/auth.module.ts
+++ libs/shared/src/auth/auth.module.ts
@@ -1,6 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { RmqModule } from '../rabbitmq/rmq.module';
 
-@Module({})
-export class Auth {
-    
+@Module({
+  imports: [RmqModule.register({ name: "AUTH" })],
+  exports: [RmqModule],
+})
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieParser()).forRoutes('*');
+  }
 }
